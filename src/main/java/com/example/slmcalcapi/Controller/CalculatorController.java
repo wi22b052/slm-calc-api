700e7e691ceddbc128825302a214e78b6c7c4c71
Weber_Benjamin <wi22b052@technikum-wien.at>
2023-05-31 20:11:56 +0200

    add add feature

---
+++ src/main/java/com/example/slmcalcapi/Controller/CalculatorController.java
@@ -0,0 +1,15 @@
+package com.example.slmcalcapi.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @RequestMapping("/calc/add")
+    public int add(@RequestParam int a,
+                   @RequestParam int b){
+        return a +b;
+    }
+}
