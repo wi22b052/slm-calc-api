97b3add7baf2ce2e03505de0dd0ea0cdfe067579
Weber_Benjamin <wi22b052@technikum-wien.at>
2023-05-31 19:30:30 +0200

    inital commit

---
+++ src/main/java/com/example/slmcalcapi/SlmCalcApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmcalcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmCalcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmCalcApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmcalcapi/SlmCalcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmcalcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmCalcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
